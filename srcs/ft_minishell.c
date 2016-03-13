a296bf6642f2c22c8b531493884348e9bef92cb8
nyhu <toussaint.boos@gmail.com>
2016-03-13 23:33:56 +0100

    work in process

--- srcs/ft_minishell.c
+++ srcs/ft_minishell.c
@@ -1,7 +1,12 @@
 # include "minishell.h"
 
+int		ft_free_error(t_config *config)
+
 int		ft_minishell(t_config *config)
 {
-	
-	while 
+	pid_t	father;
+
+	ft_putstr(config->prompt);
+	if (0 > (father = fork())
+		return (ft_fork_error(config));
 }
--- srcs/main.c
+++ srcs/main.c
@@ -1,16 +1,28 @@
 #include "minishell.h"
 
+static int		ft_initerror(void)
+{
+		ft_putstr_fd("minishell: init error", 2);
+		return (1);
+}
+
 int		main(char **env)
 {
 	t_config	*config;
 	int			i;
 
 	if (!(config = (t_config *)ft_memalloc(sizeof(t_config))))
+		return (ft_initerror());
+	if (!(config->env = ft_strtabdup(env)))
 	{
-		ft_putstr("minishell: init error");
-		return (1);
+		free(config);
+		return (ft_initerror());
 	}
-	config->env = ft_strtabdup(env);
-	config->prompt = ft_strdup("&> ");
+	if (!(config->prompt = ft_strdup("&> ")))
+	{
+		ft_strtabfree(config->env);
+		free(config);
+		return (ft_initerror());
+	}
 	return (ft_minishell(config));
 }
