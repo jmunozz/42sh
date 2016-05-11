e50664624bc5917c28c24560fb87b53cd6104ad4
Toussaint BOOS <tboos@e2r9p9.42.fr>
2016-05-11 14:56:01 +0200

    ajout header keys mac et linux

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tboos <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/17 15:23:28 by tboos             #+#    #+#             */
-/*   Updated: 2016/05/11 14:26:57 by tboos            ###   ########.fr       */
+/*   Updated: 2016/05/11 14:54:45 by tboos            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,13 @@
 # include <fcntl.h>
 # include <signal.h>
 # include "libft.h"
+# ifdef LINUX
+#  include "keys_linux.h"
+# elif
+#  include "key_mac.h"
+# endif
 # define HISTORY_SIZE 256
 # define FT_PUTSTRFD ft_putstr_str_str_fd
-# define CLF 0x0A //\n
-# define SUP 0x7E335B1B //sup
-# define CHT 0x09 //\t
-# define DEL 0x7F //DEL
-# define LEF 0x445B1B //left
-# define RIG 0x435B1B //right
-# define UPP 0x415B1B //up
-# define DOW 0x425B1B //down
-# define CLEF 0x44353B315B1B //CTRL left
-# define CRIG 0x43353B315B1B //CTRL up
-# define CUPP 0x41353B315B1B //CTRL right
-# define CDOW 0x42353B315B1B //CTRL down
-# define END 0x464F1B //end
-# define HOM 0x484F1B //home
-# define NUL 0x00 //\0
 # define QUOTE_ERR "please end your quote before running command"
 # define PAR_ERR "missing '(' ')' '[' ']' '{' '}' character"
 # define BACK_ERR "missing character after backslash"
