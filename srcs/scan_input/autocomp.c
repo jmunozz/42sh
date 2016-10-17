0c5a70b634bedd04118534b5a22cc2ddfff1d1f4
Jordan MUNOZ <jmunoz@e2r8p10.42.fr>
2016-10-17 16:46:37 +0200

    debut tests

--- srcs/scan_input/autocomp.c
+++ srcs/scan_input/autocomp.c
@@ -6,13 +6,41 @@
 /*   By: tboos <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/11 14:29:29 by tboos             #+#    #+#             */
-/*   Updated: 2016/05/11 14:31:13 by tboos            ###   ########.fr       */
+/*   Updated: 2016/10/17 16:46:05 by jmunoz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*
+   static void	get_autocomp(t_stream *stream)
+   {
+   }
+
+   static char	*ft_last_separators(const char *str, char c)
+   {
+   int i;
+   char *ret;
+
+   i = 0;
+   if (!str || !*str)
+   return (NULL);
+   while (str[i])
+   i++;
+   if (str[--i] == c)
+   return (NULL);
+   if ((ret = ft_strrchr(str, c)))
+   return (ret + 1);
+   return (NULL);
+   }
+   */
 void	ft_autocomp(t_stream *stream)
 {
-	ft_bzero(stream->buf, 4);
+	int		buf_pos;
+
+	buf_pos = stream->pos;
+	if (stream->command)
+		while (stream->command[stream->pos] != ' ' && stream->command[stream->pos])
+			ft_mvright(stream);
 }
+
