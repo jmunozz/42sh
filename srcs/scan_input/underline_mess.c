9071ad2a552a71cfffe214831771d3220cbd5ad4
Jordan MUNOZ <jmunoz@e2r8p13.42.fr>
2016-10-25 16:34:43 +0200

    explications

--- srcs/scan_input/underline_mess.c
+++ srcs/scan_input/underline_mess.c
@@ -4,21 +4,21 @@ static void	ft_putmess(t_stream *stream, char *mess)
 {
 	size_t			i;
 
-	ft_putstr(mess);
+	ft_putstr(mess);//ecrit le message
 	if (!((stream->pos + stream->config->prompt_len) % stream->col))
 	{
 		ft_putstr(" ");
 		stream->tput = "le";
 		ft_tputs(stream);
 	}
-	stream->tput = "le";
-	i = stream->col;
+	stream->tput = "le"; //move the cursor left one column
+	i = stream->col; 
 	while (i--)
 		ft_tputs(stream);
-	i = ft_strlen(mess) / stream->col + 1;
-	stream->tput = "up";
+	i = ft_strlen(mess) / stream->col + 1; // trouve le nb de lignes ecrites
+	stream->tput = "up"; //remonte le curseur d'une ligne
 	while (i--)
-		ft_tputs(stream);
+		ft_tputs(stream); //replace le curseur sur la bonne ligne
 
 }
 
@@ -27,12 +27,12 @@ int			ft_underline_mess(char *mess, t_stream *stream)
 	size_t			i;
 	unsigned int	pos_buf;
 
-	pos_buf = stream->pos;
-	ft_goend(stream);
+	pos_buf = stream->pos; //sauvegarde la pos sur stream (modifiee dans go_end)
+	ft_goend(stream); // va jusqu'a la fin de la ligne
 	stream->tput = "do"; // move the cursor one line down
 	ft_tputs(stream);
 	stream->tput = "le"; // move the cursor left one column
-	i = stream->col;
+	i = stream->col // nb de colonnes dans le terminal
 	while (i--)
 		ft_tputs(stream);
 	stream->tput = "cd"; //clear line cursor and all lines below.
@@ -42,10 +42,10 @@ int			ft_underline_mess(char *mess, t_stream *stream)
 		i = stream->config->prompt_len + ft_strlen(stream->command);
 	else
 		i = stream->config->prompt_len;
-	i = i % stream->col;
-	stream->tput = "nd";
+	i = i % stream->col; // calcule le nb de colonnes ecrites sur la derniere ligne
+	stream->tput = "nd"; //move the cursor right one column 
 	while (i--)
-		ft_tputs(stream);
-	ft_gomatch(stream, pos_buf, ft_mvleft);
+		ft_tputs(stream); //retourn au bout la ligne
+	ft_gomatch(stream, pos_buf, ft_mvleft); //replace le curseur a sa place d'origine
 	return (0);
 }
