85445830500f1d565f4eb6ae088267fc28a5fc31
Dflk <baran.romain@gmail.com>
2016-11-09 21:12:10 +0100

    env update

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,11 +6,24 @@
 /*   By: rbaran <rbaran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 14:14:44 by rbaran            #+#    #+#             */
-/*   Updated: 2016/11/09 19:48:04 by rbaran           ###   ########.fr       */
+/*   Updated: 2016/11/09 21:11:38 by rbaran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	ft_freeinenv(char **argv)
+{
+	char	**next;
+
+	printf("--------freeinenv---------\n");
+	printf("Env value: %s\n", *argv);
+	next = argv + 1;
+	ft_freegiveone((void**)argv);
+	*argv = *next;
+	printf("Next value: %s\n", *argv);
+	printf("--------end freeinenv---------\n");
+}
 
 static void	ft_printhelp(void)
 {
@@ -21,10 +34,8 @@ static void	ft_printhelp(void)
 static void	ft_parseparams(char **argv, int *param, t_config *config)
 {
 	char	*unset[3];
-	char	**next;
 
 	unset[2] = NULL;
-	next = argv;
 	while (*argv && (*argv)[0] == '-')
 	{
 		if ((*argv)[1] == 'h' || !ft_strcmp(*argv, ENV_HELP))
@@ -35,31 +46,24 @@ static void	ft_parseparams(char **argv, int *param, t_config *config)
 		{
 			unset[1] = *(argv + 1);
 			ft_unsetenv((char**)unset, config);
-			ft_freegiveone((void**)*argv);
-			next = argv + 1;
-			*argv = *next;
+			ft_freeinenv(argv);
 		}
 		else
 			break ;
-		next = argv + 1;
-		ft_freegiveone((void**)*argv);
-		*argv = *next;
+		ft_freeinenv(argv);
 	}
 }
 
 static void	ft_createenv(char **argv, t_config *config)
 {
 	char	*equal;
-	char	**next;
 
 	while (*argv && (equal = ft_strchr(*argv, '=')))
 	{
-		next = argv + 1;
 		*equal = '\0';
 		ft_setenv(*argv, equal + 1, config);
 		ft_putstrtab(config->env, '\n');
-		ft_freegiveone((void**)*argv);
-		*argv = *next;
+		ft_freeinenv(argv);
 	}
 }
 
@@ -67,12 +71,16 @@ int			ft_env(char **argv, t_config *config)
 {
 	int		param;
 
+	printf("---------ENV----------\n");
 	if (!argv[1])
 	{
 		ft_putstrtab((config->env), '\n');
 		ft_putchar('\n');
 		return (1);
 	}
+	printf("Env value: %s\n", *argv);
+	ft_freeinenv(argv);
+	printf("Env new value: %s\n", *argv);
 	param = 0;
 	ft_parseparams(argv, &param, config);
 	if (param & ENV_H)
