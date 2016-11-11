1d239733f57ea5ac8f71206c1348ff10c1dc1ea2
Martingale <j1805@hotmail.com>
2016-11-11 17:06:29 +0100

    ajout underline

--- srcs/scan_input/underline_mess.c
+++ srcs/scan_input/underline_mess.c
@@ -1,31 +1,51 @@
-#include "../../includes/minishell.h"
+#include "minishell.h"
 
+static void	ft_putmess(t_stream *stream, char *mess)
+{
+	size_t			i;
+
+	ft_putstr(mess);
+	if (!((stream->pos + stream->config->prompt_len) % stream->col))
+	{
+		ft_putstr(" ");
+		stream->tput = "le";
+		ft_tputs(stream);
+	}
+	stream->tput = "le";
+	i = stream->col;
+	while (i--)
+		ft_tputs(stream);
+	i = ft_strlen(mess) / stream->col + 1;
+	stream->tput = "up";
+	while (i--)
+		ft_tputs(stream);
+
+}
 
 int			ft_underline_mess(char *mess, t_stream *stream)
 {
 	size_t			i;
 	unsigned int	pos_buf;
 
-	mess = NULL;
 	pos_buf = stream->pos;
-	ft_goend(stream); //on va au bout de la ligne de commande.
-	stream->tput = "do"; //on saute une ligne.
+	ft_goend(stream);
+	stream->tput = "do";
 	ft_tputs(stream);
 	stream->tput = "le";
 	i = stream->col;
 	while (i--)
-		ft_tputs(stream); //ATTENTION peut-être une incohérence.
-	stream->tput = "cd"; //clear line cursor and all lines below.
+		ft_tputs(stream);
+	stream->tput = "cd";
 	ft_tputs(stream);
-	ft_comp_print(stream);
+	ft_putmess(stream, mess);
 	if (stream->command)
-		i = stream->config->prompt_len + ft_strlen(stream->command); // i = prompt + commande
+		i = stream->config->prompt_len + ft_strlen(stream->command);
 	else
-		i = stream->config->prompt_len; // si pas de comande i = prompt
-	i = i % stream->col; // retranche ce qui est du à la présence de plusieurs lignes.
+		i = stream->config->prompt_len;
+	i = i % stream->col;
 	stream->tput = "nd";
 	while (i--)
-		ft_tputs(stream); //on se retrouve sur le dernier caractère de la ligne de commande.
-	ft_gomatch(stream, pos_buf, ft_mvleft); //on remonte jusqu'au caractère enregistré.
+		ft_tputs(stream);
+	ft_gomatch(stream, pos_buf, ft_mvleft);
 	return (0);
 }
