79f3b0b66cedd3883cecd0274aeda6ff267b3942
nyhu <toussaint.boos@gmail.com>
2016-11-13 21:09:09 +0100

    fg a débug

--- srcs/builtins/jobs.c
+++ srcs/builtins/jobs.c
@@ -8,23 +8,24 @@ static t_list	*ft_extract_job(t_config *config, char *description)
 
 	p = config->jobs;
 	i = 1;
-	if (!description || ft_cmp_jobs(config->jobs->data, description, i))
+	if (!description || ft_cmp_jobs(p->data, description, i))
 	{
 		m = (t_list*)p->data;
-		config->jobs = config->jobs->next;
+		config->jobs = p->next;
 		free(p);
 		return (m);
 	}
-	while ((m = p) && (p = p->next) && ++i)
-	{
-		if (ft_cmp_jobs(p->data, description, i))
+	else
+		while ((m = p) && (p = p->next) && ++i)
 		{
-			m->next = p->next;
-			m = (t_list*)p->data;
-			free(p);
-			return (m);
+			if (ft_cmp_jobs(p->data, description, i))
+			{
+				m->next = p->next;
+				m = (t_list*)p->data;
+				free(p);
+				return (m);
+			}
 		}
-	}
 	return (NULL);
 }
 
@@ -33,10 +34,12 @@ static void	ft_continue(t_config *config, char *description, int mode)
 	t_list	*p;
 	t_list	*target;
 
-	if (!description && !(config->jobs))
-		ft_error("fg", NULL, "no current jobs", CR_ERROR);
+	if (!(config->jobs))
+		ft_error(mode == JOBS_FG ? "fg" : "bg", NULL, "no current jobs",
+		CR_ERROR);
 	else if (!(target = ft_extract_job(config, description)))
-		ft_error("fg", "no current job", description, CR_ERROR);
+		ft_error(mode == JOBS_FG ? "fg" : "bg", description, "no such job",
+		CR_ERROR);
 	else if (mode == JOBS_FG)
 	{
 		p = target;
@@ -52,12 +55,17 @@ void		ft_jobs(char **argv, t_config *config)
 
 	i = 1;
 	ft_print_jobs(NULL, argv[i]);
-	if (!argv[i])
-		ft_print_list(config->jobs);
+	if (!config->jobs)
+	{
+		ft_error("jobs", NULL, "no current job", CR_ERROR);
+		return ;
+	}
+	else if (!argv[i])
+		ft_lstiter(config->jobs, ft_print_list);
 	else
 		while (argv[i++])
 		{
-			ft_print_list(config->jobs);
+			ft_lstiter(config->jobs, ft_print_list);
 			ft_print_jobs(NULL, argv[i]);
 		}
 
@@ -71,6 +79,6 @@ void		ft_fgbg(char **argv, t_config *config, int mode)
 	if (!argv[i])
 		ft_continue(config, NULL, mode);
 	else
-		while (argv[i++])
-			ft_continue(config, argv[i], mode);
+		while (argv[i])
+			ft_continue(config, argv[i++], mode);
 }
--- srcs/builtins/jobs_utils.c
+++ srcs/builtins/jobs_utils.c
@@ -11,7 +11,7 @@ int		ft_cmp_jobs(t_list *s, char *description, int i)
 	{
 		if (ft_isdigit(*description) && ft_atoi(description) == i)
 			return (1);
-		if (!ft_strstr(description, sentence))
+		else if (ft_strstr(description, sentence))
 			return (1);
 	}
 	else if (!ft_strncmp(description, sentence, ft_strlen(description)))
@@ -22,20 +22,28 @@ int		ft_cmp_jobs(t_list *s, char *description, int i)
 void	ft_print_jobs(t_list *sentence, char *description)
 {
 	static char	*desc = NULL;
+	static char	found = 1;
 	static int	i = 0;
 
 	if (!sentence)
 	{
+		if (!found && desc)
+			ft_error("jobs", desc, "no such job", CR_ERROR);
+		ft_freegiveone((void**)&desc);
 		i = 1;
-		desc = description;
+		found = 0;
+		if (description)
+			desc = ft_strdup(description);
 	}
 	else
 	{
-		if (!desc || ft_cmp_jobs(sentence, desc, i))
+		if (ft_cmp_jobs(sentence, desc, i))
 		{
 			FT_PUTSTRFD("[", ft_st_itoa(i), "]       ", 1);
-			FT_PUTSTRFD(ft_st_itoa(*((int*)sentence->next->data)), "    ", sentence->data, 1);
+			FT_PUTSTRFD(ft_st_itoa(*((int*)sentence->next->data)), "    ",
+				sentence->data, 1);
 			ft_putchar('\n');
+			found = 1;
 		}
 		i++;
 	}
