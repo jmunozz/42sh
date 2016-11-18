8058b9d88f2e5e7c4fc0a67dd3b1c68bafeba40c
Toussaint BOOS <tboos@e1r3p19.42.fr>
2016-11-18 14:35:29 +0100

    mise a la norme de la libft

--- libft/list/ft_list_find.c
+++ libft/list/ft_list_find.c
@@ -6,7 +6,7 @@
 /*   By: tboos <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/05 22:56:43 by tboos             #+#    #+#             */
-/*   Updated: 2015/11/06 00:16:40 by tboos            ###   ########.fr       */
+/*   Updated: 2016/11/18 14:29:39 by tboos            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ t_dclist		*ft_dclist_find(t_dclist *begin_list, void *data_ref,
 	return (NULL);
 }
 
-t_list		*ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)())
+t_list			*ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)())
 {
 	while (begin_list)
 	{
