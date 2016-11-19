0023afe9a80621d12ed446648536eb8bb596cf0f
maxpetit <piccolo.maxime@gmail.com>
2016-11-19 12:48:11 +0100

    pret a merge

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -12,50 +12,6 @@
 
 #include "minishell.h"
 
-<<<<<<< HEAD:srcs/scan_input/autocomp.c
-static int ft_check_marge(char c)
-{
-	if (c == '<' || c == '>' || c == '&' || c == '|' || c == ';'|| c == ' ')
-		return (1);
-	return (0);
-}
-
-static char *ft_get_path(t_stream *stream, t_globing *data)
-{
-	int		i;
-	int		j;
-	int		len;
-	char	*path;
-
-	i = stream->pos;
-	len = 0;
-	while (i >= 0 && (!ft_check_marge(stream->command[i])))
-		i--;
-	j = ++i;
-	while (stream->command[i] && (!ft_check_marge(stream->command[i])) && ++len)
-		i++;
-	data->end = i;
-	path = ft_strnew(len);
-	path = ft_strncpy(path, &(stream)->command[j], len);
-	stream->pos = j;
-	if ((ft_strstri(path, "*")) != -1)
-		return (path);
-	return (NULL);
-}
-
-void	ft_autocomp(t_stream *stream)
-{
-	t_globing	glob;
-	int			end;
-
-	ft_bzero(stream->buf, 4);
-	ft_bzero(&glob, sizeof(void*));
-	if ((glob.path = ft_get_path(stream, &glob)))
-		ft_loop_path(stream, &glob);
-	else
-		ft_underline_mess("NO", stream);
-	ft_strdel(&glob.path);
-=======
 static int	ft_parseparams(char **argv, int *param)
 {
 	int	i;
@@ -81,5 +37,4 @@ void	ft_echo(char **argv)
 	}
 	if (!(param & ECHO_N))
 		ft_putchar('\n');
->>>>>>> tboos/master:srcs/builtins/echo.c
 }
