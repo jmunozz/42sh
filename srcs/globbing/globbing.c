c35dbcb31c3c591a51cf65905e40b910753f1b23
Jordan MUNOZ <jmunoz@e1r6p10.42.fr>
2016-11-22 20:34:35 +0100

    tests - 2

--- srcs/globbing/globbing.c
+++ srcs/globbing/globbing.c
@@ -1,5 +1,19 @@
 #include "../../includes/minishell.h"
 
+char	*ft_strnchr(const char *s, int c, size_t size)
+{
+	int i;
+
+	if (size)
+	{
+		i = 0;
+		while (s[i] && s[i] != (char)c && i < size - 1)
+			i++;
+		if (s[i] == (char)c)
+			return ((char*)&s[i]);
+	}
+	return (NULL);
+}
 
 int		ft_strlenc(char *str, char c)
 {
@@ -19,12 +33,41 @@ int		ft_strlenc(char *str, char c)
 
 int	is_alphabefore(int j, char *file)
 {
-	while (--j && !ft_isalpha(file[j]))
+	while (--j >= 0 && !ft_isalpha(file[j]))
 	{
 		if (file[j] == '*')
 			return (0);
 	}
 	return (1);
+}
+
+int		brackets(int *i, char *glob, char c)
+{
+	int			min;
+	int			max;
+	int			match;
+
+	match = 0;
+	if (!ft_strchr(&glob[*i], ']'))
+		return (0);
+	while (glob[*i] != ']' && !match)
+	{
+		if (glob[*i + 1] && glob[*i + 2] && glob[*i + 1] == '-' && glob[*i + 2] != ']')
+		{
+			min = (int)glob[*i] - 1;
+			max = (int)glob[*i + 2];
+			while (++min <= max && !match)
+				if ((int)c == min)
+					match = 1;
+			(*i) += 2;
+		}
+		else if (glob[*i] == c)
+			match = 1;
+		(*i)++;
+	}
+	while (glob[*i] != ']')
+		(*i)++;
+	return (match);
 }
 
 int  is_valid(int i, int j, char *glob, char *file)
@@ -65,14 +108,25 @@ int  is_valid(int i, int j, char *glob, char *file)
 		}
 		return (0);
 	}
+	else if (glob[i] == '[')
+	{
+		if (brackets(&i, glob, file[j]))
+			return(is_valid(++i, ++j, glob, file));
+		return (0);
+	}
 	else if (ft_isalpha(glob[i]))
 	{
+
+		printf("isalpha\n");
 		if (glob[i] == file[j])
 			return(is_valid(++i, ++j, glob, file));
 		return (0);
 	}
 	else if (glob[i] == '*')
+	{
+		printf("*\n");
 		return (is_valid(++i, j, glob, file));
+	}
 	else
 		return (0);
 }
@@ -98,22 +152,26 @@ int		main(int ac, char **av)
 	t_list		*list;
 	t_list		*begin;
 
-	ft_list_push_back(&begin, ft_lstnew(test1, 0));
+	//ft_list_push_back(&begin, ft_lstnew(test1, 0));
 	//ft_list_push_back(&begin, ft_lstnew(test2, 0));
 	//ft_list_push_back(&begin, ft_lstnew(test3, 0));
 	ft_list_push_back(&begin, ft_lstnew(test4, 0));
 	//ft_print_list(begin);
-	if (ac)
+	if (ac > 1)
 	{
 		list = begin;
 		printf("Execution fonction globbing...\n");
 		while (list)
 		{
+			printf("Liste traitee = %s\n", list->data);
 			list->data_size = (size_t)is_valid(0, 0, av[1], list->data);
 			list = list->next;
 		}
 		ft_print_list(begin);
 	}
+	/*printf("%d\n", brackets(&i, av[2], av[3][0]));
+	printf("new i = %d\n", i);
+	printf("%c\n", av[2][i]);*/
 	return (0);
 }
 
